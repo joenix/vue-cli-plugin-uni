f3c80c630bb7a495976d6327722d5363afdfae64
Joenix <joenix@qq.com>
2021-10-31 15:00:53 +0800

    feat: new version

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Path
 const path = require("path");
 
+// Fs
+const fs = require("fs");
+
 // Utils
 const {
   log,
@@ -15,6 +18,12 @@ const {
 // Root
 const root = process.cwd();
 
+// Conf
+const conf = getConf();
+
+// Argv
+const argvs = getArgv();
+
 /**
  * Path Resolve
  * @param dir {string}
@@ -25,13 +34,63 @@ function resolve(dir) {
 }
 
 /**
+ * Foreach
+ * @param source {json}
+ * @param callback {function}
+ * ======== ======== ========
+ */
+function foreach(source = {}, callback = () => {}) {
+  Object.keys(source).map(item => callback(source[item], item));
+}
+
+/**
+ * Check
+ * @param value {any}
+ * @param mode {any}
+ * ======== ======== ========
+ */
+function check(value, mode = Object) {
+  return value && value.constructor === mode;
+}
+
+/**
+ * Deep
+ * @param source {json}
+ * @param callback {function}
+ * ======== ======== ========
+ */
+function deep(source, callback) {
+  // Check
+  if (check(source)) {
+    // Recursion
+    foreach(source, value => deep(value, callback));
+    // Factory
+    source = callback(source);
+  }
+}
+
+/**
+ * Get Configure
+ * ======== ======== ========
+ */
+function getConf(path = `${root}/scaff.config2.js`) {
+  // If File
+  return fs.existsSync(path) ? require(path) : {};
+}
+
+/**
  * Get Argv in Process
  * @param name {string}
  * ======== ======== ========
  */
 function getArgv(name, cache = {}) {
-  // Argv
-  const [node, path, ...argv] = process.argv;
+  // Argv -- Cant Run in Plugign
+  // let [node, path, ...argv] = process.argv;
+
+  // Get Argv From Original Command
+  let argv = JSON.parse(process.env.npm_config_argv).original.filter(cmd =>
+    /^--/.test(cmd)
+  );
 
   // Mapping
   argv.map(
@@ -55,22 +114,51 @@ function getArgv(name, cache = {}) {
  */
 function toStringify(conf = {}) {
   // Machining
-  return Object.keys(conf).map(key => (conf[key] = `'${conf[key]}'`)), conf;
+  deep(conf, json => {
+    for (let i in json) {
+      if (json[i].constructor !== Object) {
+        json[i] = `'${json[i]}'`;
+      }
+    }
+  });
+
+  // Return
+  return conf;
+}
+
+/**
+ * To Stringify
+ * @param conf {json}
+ * ======== ======== ========
+ */
+function getProp(fn) {
+  return `"${fn(process.env)}"`;
+}
+
+// Argvs injection must run front at Vue
+if (argvs.env) {
+  process.env.NODE_ENV = argvs.env;
 }
 
 // Action
 module.exports = (api, options, rootOptions) => {
+  // Merge User Config 2 Project Options
+  Object.assign(api.service.projectOptions, conf);
+
   // Use chainWebpack
   api.chainWebpack(webpackConfig => {
     // Configure
-    const configure = require(`${root}/configure.json`);
-    // Argvs
-    const argvs = getArgv();
+    const injection = require(`${root}/injection.json`);
 
-    // Env Default
-    argvs.env = argvs.env || `"dev"`;
-    // Plat Default
-    argvs.plat = argvs.plat || `"${process.env.VUE_APP_PLATFORM}"`;
+    // Environ
+    if (argvs.env === undefined) {
+      argvs.env = getProp(e => e.npm_config_env || e.NODE_ENV || "development");
+    }
+
+    // Platform
+    if (argvs.plat === undefined) {
+      argvs.plat = getProp(e => e.VUE_APP_PLATFORM);
+    }
 
     // Injection
     webpackConfig.plugin("define").tap(
@@ -79,7 +167,7 @@ module.exports = (api, options, rootOptions) => {
         // Merge to Env
         Object.assign(definitions[0]["process.env"], {
           ...argvs,
-          ...toStringify(configure)
+          ...toStringify(injection)
         }),
         // Return
         definitions
